edd4cf452f2f1522ab958cb0d87f6ca2b5c4a415
Ivan I. Ovchinnikov <mail@iovchinnikov.ru>
2018-08-02 16:51:20 +0300

    Корректное разбиение доходов и расходов по экранам и дефолт категория "начальный остаток"

--- modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionEdit.java
+++ modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionEdit.java
@@ -1,9 +1,6 @@
 package ru.iovchinnikov.yaap.web.transaction;
 
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import com.haulmont.cuba.gui.components.Field;
-import com.haulmont.cuba.gui.components.FieldGroup;
-import com.haulmont.cuba.gui.components.ValidationException;
+import com.haulmont.cuba.gui.components.*;
 import ru.iovchinnikov.yaap.entity.Transaction;
 
 import javax.inject.Inject;
@@ -11,12 +8,34 @@ import javax.inject.Named;
 import java.util.Map;
 
 public class TransactionEdit extends AbstractEditor<Transaction> {
+    public static final int MODE_IN = 1;
+    public static final int MODE_OUT = 0;
+
     @Named("mainGroup.fTotal") private Field fTotal;
+    @Named("mainGroup.fAmount") private Field fAmount;
+    @Named("mainGroup.fCat") private PickerField fCat;
+
+    private int mode;
 
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
         fTotal.addValidator(value -> nonZero((double) value));
+        if (((Transaction) params.get("ITEM")).getDirection()) {
+            mode = MODE_IN;
+            fCat.getLookupAction().setLookupScreen("yaap$CatIncome.browse");
+            fAmount.setVisible(false);
+        } else {
+            mode = MODE_OUT;
+            fCat.getLookupAction().setLookupScreen("yaap$CatSpend.browse");
+        }
+    }
+
+    @Override
+    public void ready() {
+        super.ready();
+        if (mode == MODE_IN)
+            fAmount.setValue(1);
     }
 
     private void nonZero(double value) throws ValidationException {
@@ -24,4 +43,6 @@ public class TransactionEdit extends AbstractEditor<Transaction> {
             throw new ValidationException(getMessage("notZero"));
         }
     }
+
+
 }
--- modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionIncome.java
+++ modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionIncome.java
@@ -1,6 +1,18 @@
 package ru.iovchinnikov.yaap.web.transaction;
 
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.actions.CreateAction;
+
+import javax.inject.Named;
+import java.util.Map;
 
 public class TransactionIncome extends AbstractLookup {
+    @Named("transactionsTable.create") private CreateAction transactionsTableCreate;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        transactionsTableCreate.setInitialValues(ParamsMap.of("direction", true));
+    }
 }
