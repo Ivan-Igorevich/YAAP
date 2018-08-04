404b1d054b61ab307aa048e3f21c84fe6f843663
OVAnechka <ocrio.box@gmail.com>
2018-08-04 15:51:47 +0300

    Подсветка транзакций

--- modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionBrowse.java
+++ modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionBrowse.java
@@ -1,6 +1,23 @@
 package ru.iovchinnikov.yaap.web.transaction;
 
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.GroupTable;
+import ru.iovchinnikov.yaap.entity.Transaction;
+
+import javax.inject.Inject;
 
 public class TransactionBrowse extends AbstractLookup {
+    @Inject
+    private GroupTable<Transaction> transactionsTable;
+
+    @Override
+    public void ready() {
+        super.ready();
+        transactionsTable.setStyleProvider((transaction, s) -> {
+            if (transaction.getDirection())
+                return "income";
+            else
+                return "spend";
+        });
+    }
 }
