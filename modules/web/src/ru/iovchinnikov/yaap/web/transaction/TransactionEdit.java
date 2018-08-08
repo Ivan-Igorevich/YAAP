78902102d26c039343966baf6d5e064e41c2da82
OVAnechka <ocrio.box@gmail.com>
2018-08-08 21:12:49 +0300

    Незначительные правки внешнего вида, переименования кнопок и колонок таблиц. Переименование страницы приложения и смена порта. Отказ от создания единичной операции  для расхода

--- modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionEdit.java
+++ modules/web/src/ru/iovchinnikov/yaap/web/transaction/TransactionEdit.java
@@ -2,29 +2,28 @@ package ru.iovchinnikov.yaap.web.transaction;
 
 import com.haulmont.cuba.core.global.TimeSource;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import ru.iovchinnikov.yaap.entity.CatIncome;
+import ru.iovchinnikov.yaap.entity.CatSpend;
 import ru.iovchinnikov.yaap.entity.Transaction;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 
 public class TransactionEdit extends AbstractEditor<Transaction> {
-    public static final int MODE_IN = 1;
-    public static final int MODE_OUT = 0;
-
     @Inject private TimeSource timeSource;
     @Named("mainGroup.fTotal") private Field fTotal;
     @Named("mainGroup.fAmount") private Field fAmount;
     @Named("mainGroup.fCat") private PickerField fCat;
     @Named("mainGroup.fDate") private Field fDate;
-
-    private int mode;
+    @Named("mainGroup.fName") private Field fName;
 
     @Override
     protected void initNewItem(Transaction item) {
         super.initNewItem(item);
-//        fDate.setValue(timeSource.currentTimestamp());
         item.setDate(timeSource.currentTimestamp());
     }
 
@@ -33,27 +32,23 @@ public class TransactionEdit extends AbstractEditor<Transaction> {
         super.init(params);
         fTotal.addValidator(value -> nonZero((double) value));
         fDate.addValidator(value -> isEarlier((Date) value));
-        if (((Transaction) params.get("ITEM")).getDirection()) {
-            mode = MODE_IN;
-            fCat.getLookupAction().setLookupScreen("yaap$CatIncome.browse");
-            fAmount.setVisible(false);
-        } else {
-            mode = MODE_OUT;
-            fCat.getLookupAction().setLookupScreen("yaap$CatSpend.browse");
-        }
+        fCat.getLookupAction().setLookupScreen("yaap$CatIncome.browse");
     }
 
     @Override
     public void ready() {
         super.ready();
-        if (mode == MODE_IN) fAmount.setValue(1);
+        fAmount.setValue(1);
+        fName.setValue(" ");
     }
 
     private void nonZero(double value) throws ValidationException {
-        if (value == 0) throw new ValidationException(getMessage("notZero"));
+        if (value == 0)
+            throw new ValidationException(getMessage("notZero"));
     }
 
     private void isEarlier(Date date) throws ValidationException {
-        if (date.after(timeSource.currentTimestamp())) throw new ValidationException(getMessage("onlyPast"));
+        if (date.after(timeSource.currentTimestamp()))
+            throw new ValidationException(getMessage("onlyPast"));
     }
 }
